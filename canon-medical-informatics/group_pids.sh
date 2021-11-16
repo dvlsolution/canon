db2de52a6d24eca908b59fce8a66e622a34bdf76
dvlsolution <dvl.solutions.inc@gmail.com>
2021-11-16 15:57:08 -0500

    Reduce memory allocation

--- canon-medical-informatics/group_pids.sh
+++ canon-medical-informatics/group_pids.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-JAVA_OPTS="$JAVA_OPTS -server -Xms2G -Xmx2G -XX:+AlwaysPreTouch -XX:+OptimizeStringConcat -XX:+UseStringDeduplication -XX:+UseCompressedClassPointers -XX:+UseCompressedOops -Djava.awt.headless=true"
+JAVA_OPTS="$JAVA_OPTS -server -Xms1G -Xmx1G -XX:+AlwaysPreTouch -XX:+OptimizeStringConcat -XX:+UseStringDeduplication -XX:+UseCompressedClassPointers -XX:+UseCompressedOops -Djava.awt.headless=true"
 $JAVA_HOME/bin/java $JAVA_OPTS -cp ./target/classes canon.medical.informatics.ParsePatientName $@
