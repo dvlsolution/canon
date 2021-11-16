b40b7a6063a7a1c4df1bf2b4aeabb6769e1426ea
dvlsolution <dvl.solutions.inc@gmail.com>
2021-11-16 15:39:00 -0500

    Removed compilation step

--- canon-medical-informatics/group_pids.sh
+++ canon-medical-informatics/group_pids.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-mvn compile
-
 JAVA_OPTS="$JAVA_OPTS -server -Xms2G -Xmx2G -XX:+AlwaysPreTouch -XX:+OptimizeStringConcat -XX:+UseStringDeduplication -XX:+UseCompressedClassPointers -XX:+UseCompressedOops -Djava.awt.headless=true"
 $JAVA_HOME/bin/java $JAVA_OPTS -cp ./target/classes canon.medical.informatics.ParsePatientName $@
